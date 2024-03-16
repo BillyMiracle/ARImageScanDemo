10b5e7a0dea037b2b9f4ca78e2224886628cf400
BillyMiracle <510986792@qq.com>
2024-03-16 22:23:13 +0800

    feat: 开发AR识别2D图像功能

--- ARImageScanDemo/ARImageScanDemo/ImageScanPageViewController.swift
+++ ARImageScanDemo/ARImageScanDemo/ImageScanPageViewController.swift
@@ -9,24 +9,95 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ImageScanPageViewController: UIViewController {
+class ImageScanPageViewController: UIViewController, ARSessionDelegate {
     
-    var arscnView: ARSCNView {
-        let view = ARSCNView.init()
-        
-        return view
-    }
-    var arWordTrackingConfiguration: ARWorldTrackingConfiguration!
+    lazy var sceneView: ARSCNView = {
+        let sceneView = ARSCNView.init(frame: CGRect.init(x: 0, y: 100, width: self.view.frame.size.width, height: self.view.frame.size.height - 200))
+        sceneView.session = self.arSession
+        sceneView.delegate = self
+        sceneView.isPlaying = false
+        return sceneView
+    }()
+    
+    lazy var arSession: ARSession = {
+        let session = ARSession()
+        session.delegate = self
+        return session
+    }()
+    
+    lazy var arWordTrackingConfiguration: ARWorldTrackingConfiguration = {
+        let refImage = ARReferenceImage.init((UIImage.init(named: "bg1")?.cgImage)!, orientation: .up, physicalWidth: 0.3)
+        refImage.name = "bg1"
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = [refImage]
+        return configuration
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
+        self.view.addSubview(sceneView)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpSceneView()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    private func setUpSceneView() {
+        let options: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
+        sceneView.session.run(arWordTrackingConfiguration, options: options)
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+}
 
-   
+extension ImageScanPageViewController: ARSCNViewDelegate {
+    public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let imageAnchor = anchor as? ARImageAnchor, let imageName = imageAnchor.referenceImage.name
+        else { return }
+        
+        if imageName == "bg1" {
+            print("识别出来了 bg1")
+            
+            let width = CGFloat(imageAnchor.referenceImage.physicalSize.height)
+            let height = CGFloat(imageAnchor.referenceImage.physicalSize.height)
+            let plane = SCNPlane(width: width, height: height)
+            
+            plane.materials.first?.diffuse.contents = UIColor.white
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.position = SCNVector3(0,0,0)
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+        }
+    }
+    
+    public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+        let planeNode = node.childNodes.first,
+        let plane = planeNode.geometry as? SCNPlane
+        else { return }
 
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+
+        // 3
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }
 }
 
 // 参考：https://www.jianshu.com/p/1f3adf9b3d96
--- ARImageScanDemo/ARImageScanDemo/ViewController.m
+++ ARImageScanDemo/ARImageScanDemo/ViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "ViewController.h"
+#import "ARImageScanDemo-Swift.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) ImageScanPageViewController *arViewController;
+@property (nonatomic, strong) UIButton *button;
 
 @end
 
@@ -16,6 +20,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.view addSubview:self.button];
+}
+
+- (void)presentARViewController {
+    [self presentViewController:self.arViewController animated:YES completion:nil];
+}
+
+- (UIButton *)button {
+    if (!_button) {
+        _button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [_button setTitle:@"跳转界面" forState:UIControlStateNormal];
+        [_button addTarget:self action:@selector(presentARViewController) forControlEvents:UIControlEventTouchUpInside];
+        [_button sizeToFit];
+        _button.center = self.view.center;
+    }
+    return _button;
+}
+
+- (ImageScanPageViewController *)arViewController {
+    if (!_arViewController) {
+        _arViewController = [[ImageScanPageViewController alloc] init];
+    }
+    return _arViewController;
 }
 
 
